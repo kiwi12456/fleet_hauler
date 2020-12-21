5e0188b6c25cfa5c8d7824f29b75665b23b87776
Charles Lee <charlesjhlee@gmail.com>
2020-12-22 02:31:04 +1100

    t

--- implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
+++ implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
@@ -1323,10 +1323,9 @@ itemHangarFromInventoryWindow =
         >> List.head
         >> Maybe.map .uiNode
 
-oreHoldFromInventoryWindow : EveOnline.ParseUserInterface.InventoryWindow -> Maybe UIElement
+oreHoldFromInventoryWindow : EveOnline.ParseUserInterface.InventoryWindowLeftTreeEntry -> Maybe UIElement
 oreHoldFromInventoryWindow =
-    .leftTreeEntries
-        .uiNode >> .uiNode >> EveOnline.ParseUserInterface.getAllContainedDisplayTexts >> List.any (String.toLower >> String.contains "ore hold")
+    .uiNode >> .uiNode >> EveOnline.ParseUserInterface.getAllContainedDisplayTexts >> List.any (String.toLower >> String.contains "ore hold")
 
 {-| The region of a ship entry in the inventory window can contain child nodes (e.g. 'Ore Hold').
 For this reason, we don't click on the center but stay close to the top.
