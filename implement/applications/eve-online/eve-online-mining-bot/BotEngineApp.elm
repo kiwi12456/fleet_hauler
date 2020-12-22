63b93619f3fe99d1a0e0f246eb28f5f16b093690
Charles Lee <charlesjhlee@gmail.com>
2020-12-23 02:34:30 +1100

    t

--- implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
+++ implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
@@ -506,18 +506,12 @@ inSpaceWithFleetHangarSelected context seeUndockingComplete inventoryWindowWithF
                                                                 |> Maybe.withDefault
                                                                     (endDecisionPath
                                                                         (actWithoutFurtherReadings
-                                                                            ( "Drag and drop."
-                                                                            , [[EffectOnWindow.effectsForDragAndDrop
-                                                                                { startLocation = itemInInventory.totalDisplayRegion |> centerFromDisplayRegion
-                                                                                , endLocation = oreHold.totalDisplayRegion |> centerFromDisplayRegion
-                                                                                , mouseButton = MouseButtonLeft
-                                                                                }]
-                                                                                ,   [EffectOnWindow.effectsMouseClickAtLocation MouseButtonLeft 
-                                                                                    { x = oreHold.totalDisplayRegion.x
-                                                                                    , y = oreHold.totalDisplayRegion.y
-                                                                                    }
-                                                                                    ]
+                                                                            ( "Press the 'W' key and click on the overview entry."
+                                                                            , [ [ EffectOnWindow.KeyDown EffectOnWindow.vkey_W ]
+                                                                                , overviewEntryToOrbit.uiNode |> clickOnUIElement MouseButtonLeft
+                                                                                , [ EffectOnWindow.KeyUp EffectOnWindow.vkey_W ]
                                                                                 ]
+                                                                                    |> List.concat
                                                                             )
                                                                         )
                                                                     )
