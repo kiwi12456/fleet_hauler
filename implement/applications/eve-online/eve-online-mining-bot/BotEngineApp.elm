39957c878a737d4c057a90b1b736552c5d375cd9
Charles Lee <charlesjhlee@gmail.com>
2020-12-24 02:04:23 +1100

    t

--- implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
+++ implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
@@ -1643,7 +1643,7 @@ numberOfItemsFromInventoryWindow =
         >> listDescendantsWithDisplayRegion
         >> List.filter (.uiNode >> getNameFromDictEntries >> Maybe.map ((==) "numItemsLabel") >> Maybe.withDefault False)
         >> List.head
-        >> .uiNode
+        >> Maybe.map .uiNode
         -- >> getAllContainedDisplayTexts
         -- >> List.head
 
