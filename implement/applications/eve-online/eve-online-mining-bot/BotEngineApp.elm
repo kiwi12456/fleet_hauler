42198677cd6161d6c65ab1a6614033e90acfaabc
Charles Lee <charlesjhlee@gmail.com>
2020-12-24 02:06:20 +1100

    t

--- implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
+++ implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
@@ -1644,7 +1644,7 @@ numberOfItemsFromInventoryWindow =
         >> List.filter (.uiNode >> getNameFromDictEntries >> Maybe.map ((==) "numItemsLabel") >> Maybe.withDefault False)
         >> List.head
         >> Maybe.map .uiNode
-        >> getAllContainedDisplayTexts
+        >> Maybe.andThen getAllContainedDisplayTexts
         -- >> List.head
 
 itemHangarFromInventoryWindow : EveOnline.ParseUserInterface.InventoryWindow -> Maybe UIElement
