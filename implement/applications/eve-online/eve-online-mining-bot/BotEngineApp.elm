c4ffc7e26f2455bd91e9f41aebd94ea0f0065d06
Charles Lee <charlesjhlee@gmail.com>
2020-12-24 01:50:35 +1100

    t

--- implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
+++ implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
@@ -1728,7 +1728,7 @@ getStringPropertyFromDictEntries dictEntryKey uiNode =
         |> Dict.get dictEntryKey
         |> Maybe.andThen (Json.Decode.decodeValue Json.Decode.string >> Result.toMaybe)
 
-getAllContainedDisplayTexts : EveOnline.MemoryReading.UITreeNode -> List (List String)
+getAllContainedDisplayTexts : EveOnline.MemoryReading.UITreeNode -> List String
 getAllContainedDisplayTexts uiNode =
     uiNode
         :: (uiNode |> EveOnline.MemoryReading.listDescendantsInUITreeNode)
