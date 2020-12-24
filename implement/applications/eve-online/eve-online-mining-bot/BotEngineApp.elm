00664171a77ebcffd626c036ec988f7bf152fbe6
Charles Lee <charlesjhlee@gmail.com>
2020-12-25 01:11:29 +1100

    t

--- implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
+++ implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
@@ -1534,8 +1534,6 @@ topmostAsteroidFromOverviewWindow : ReadingFromGameClient -> Maybe OverviewWindo
 topmostAsteroidFromOverviewWindow =
     overviewWindowEntriesRepresentingAsteroids
         >> List.sortBy (.uiNode >> .totalDisplayRegion >> .y)
-        >> List.range 1 5
-        >> List.reverse
         >> List.head
 
 
