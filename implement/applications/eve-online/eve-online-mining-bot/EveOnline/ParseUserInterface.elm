fe7b9adffb0500b5032976a8a5a4bdba70f36e37
Charles Lee <charlesjhlee@gmail.com>
2020-12-24 22:23:03 +1100

    t

--- implement/applications/eve-online/eve-online-mining-bot/EveOnline/ParseUserInterface.elm
+++ implement/applications/eve-online/eve-online-mining-bot/EveOnline/ParseUserInterface.elm
@@ -1721,7 +1721,7 @@ parseInventoryWindow windowUiNode =
                     (listDescendantsWithDisplayRegion
                         >> List.filter
                             (\uiNode ->
-                                [ "ShipCargo", "ShipDroneBay", "ShipOreHold", "StationItems", "ShipFleetHangar", "ItemHangar" ]
+                                [ "ShipCargo", "ShipDroneBay", "ShipOreHold", "StationItems", "ShipFleetHangar", "StructureItemHangar" ]
                                     |> List.member uiNode.uiNode.pythonObjectTypeName
                             )
                         >> List.head
