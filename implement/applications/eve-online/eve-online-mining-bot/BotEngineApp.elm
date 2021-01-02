5379fd4671af43201918066e198e3661499b5cb9
Charles Lee <charlesjhlee@gmail.com>
2021-01-03 01:14:06 +1100

    t

--- implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
+++ implement/applications/eve-online/eve-online-mining-bot/BotEngineApp.elm
@@ -20,6 +20,7 @@ import Common.Basics exposing (listElementAtWrappedIndex)
 import Common.DecisionTree exposing (describeBranch, endDecisionPath)
 import Common.EffectOnWindow as EffectOnWindow exposing (MouseButton(..))
 import Dict
+import Random
 import EveOnline.AppFramework
     exposing
         ( AppEffect(..)
